0802d24af7eab1a3853d5c54a6e55fcd73cc64e4
an2riuz <an2riuz@gmail.com>
2018-12-03 20:11:49 +0200

    Get, Set

---
+++ 11 paskaita 2018 12 03/11 paskaita 2018 12 03/Kintamieji.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_paskaita_2018_12_03
+{
+    class Kintamieji
+    {
+        private string kintamasis1 = "Labas";
+        private string kintamasis2 = "vakaras";
+        private string kintamasis3 = "stai";
+        private string kintamasis4 = "ir as";
+
+        public Kintamieji()
+        {
+            
+        }
+        public string Kintamasis1
+        {
+            get { return kintamasis1; }
+            set { kintamasis1 = value; }
+        }
+        public string Kintamasis2
+        {
+            get { return kintamasis2; }
+        }
+        public string Kintamasis3
+        {
+            set { kintamasis3 = value; }
+        }
+        public string Kintamasis4 { get; private set; }
+    }
+}
---
+++ 11 paskaita 2018 12 03/11 paskaita 2018 12 03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_paskaita_2018_12_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Kintamieji zodziai = new Kintamieji();
+            //zodziai.Kintamasis1 = "Sveikas";
+
+            //zodziai.Kintamasis3 = "va";
+            //zodziai.Kintamasis4 = "zodis";
+            Console.WriteLine(zodziai.Kintamasis1);
+            Console.WriteLine(zodziai.Kintamasis2);
+            //Console.WriteLine(zodziai.Kintamasis3);
+            Console.WriteLine(zodziai.Kintamasis4);
+            
+            Console.ReadKey();
+        }
+    }
+}
